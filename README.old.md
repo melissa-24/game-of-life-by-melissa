20e8793a84ab60af7c44dfe2134389249addaf70
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-13 21:38:57 -0400

    adding lambdas rule

--- README.old.md
+++ README.old.md
@@ -1,2 +1,7 @@
 # game-of-life-by-melissa
-Melissa's version of Conway's Game of LIfe
+Melissa's version of Conway's Game of Life
+
+## Thoughts and Ideas
+
+1. Hexagonal grid vs square
+2. Colors if hex grid =  yellow and black
--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import './App.css'
 const App = () => {
   return (
     <>
-     
+     <header>
+       <h1>Conway's Game of Life - by Melissa</h1>
+       <nav>
+       </nav>
+     </header>
+     <div className='Game'>
+       {/* <Grid /> */}
+       {/* <Rules /> */}
+     </div>
     </>
   );
 };
