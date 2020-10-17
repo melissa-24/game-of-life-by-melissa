c75f60fe71dd0439ce57d8739e4a91554ee2c3fa
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-17 00:09:24 -0400

    saving all changes

---
+++ src/components/NewGrid.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class Grid extends React.Component {
+    
+
+    render() {
+
+
+        return (
+            <>
+            <p>Game Board Grid</p>
+            <div className="the-grid">
+
+            </div>
+            </>
+        )
+    }
+}
+
+export default Grid
