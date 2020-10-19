0f073e9d8ae4272a2684b6b1ea9cb6b81c0aa099
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-19 17:38:52 -0400

    with help got cells to change not whole grid

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,6 +4,10 @@ const Rules = () => {
     return (
         <>
         <p>rules section</p>
+        <p>Fewer than 3 live neighbors dies</p>
+        <p>3, 4 or 5 neighbors lives on</p>
+        <p>More than 5 neighbors dies</p>
+        <p>4 or 5 live neighbors creates new live in dead</p>
         </>
     )
 }
